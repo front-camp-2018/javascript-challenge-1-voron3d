674a9c40abcf75766f10d220e580208719cc8622
Voron3d <Voron3d@gmail.com>
2018-11-01 21:38:42 +0200

    Done Home work #1

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const sNum = String(num);
+  const sNum1 = sNum.slice(0, sNum.length / 2);
+  const sNum2 = sNum.slice(sNum.length / 2);
+  return (sNum1 === sNum2) ? num : num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,52 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+
+  const wArr = arr.map(num => {
+    return num.split('').reduce((sum, current) => {
+      return sum + Number(current);
+    }, 0);
+  });
+
+  function swap (items, firstIndex, secondIndex) {
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+  }
+
+  function partition (items, left, right) {
+    const pivot = items[Math.floor((right + left) / 2)];
+
+    while (left <= right) {
+      while (items[left] < pivot) {
+        left++;
+      }
+      while (items[right] > pivot) {
+        right--;
+      }
+      if (left <= right) {
+        swap(items, left, right);
+        swap(arr, left, right);
+        left++;
+        right--;
+      }
+    }
+    return left;
+  }
+
+  function quickSort (items, left = 0, right = items.length - 1) {
+    if (items.length > 1) {
+      const index = partition(items, left, right);
+      if (left < index - 1) {
+        quickSort(items, left, index - 1);
+      }
+      if (index < right) {
+        quickSort(items, index, right);
+      }
+    }
+    return items;
+  }
+
+  quickSort(wArr);
+  return arr;
+
 };
